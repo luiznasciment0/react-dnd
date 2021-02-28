5c1a4921f7d382e6e81363c2c5bfc23edf652377
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-28 18:01:01 -0300

    avatarupload image update

--- src/components/AvatarUpload/image/index.tsx
+++ src/components/AvatarUpload/image/index.tsx
@@ -3,13 +3,12 @@ import React from 'react'
 import { CroppedImageWrapper } from './styles'
 
 interface Props {
-  file?: File
   error?: boolean
   bgImage: () => string
 }
 
-const Image = ({ bgImage }: Props) => {
-  return <CroppedImageWrapper backgroundImage={bgImage} />
+const Image = ({ bgImage, error }: Props) => {
+  return <CroppedImageWrapper backgroundImage={bgImage} error={error} />
 }
 
 export default Image
