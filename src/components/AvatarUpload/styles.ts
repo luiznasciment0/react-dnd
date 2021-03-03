2b5dce13649eb16a5ca0fbf8423c2d78871f07f6
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-03 01:51:59 -0300

    centralize dropzone

--- src/components/AvatarUpload/styles.ts
+++ src/components/AvatarUpload/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Dropzone = styled.div`
   padding: 2rem;
   height: 17.7rem;
-  max-width: 55.3rem;
+  width: 55.3rem;
   background: #f2f5f8;
   border: 2px dashed #c7cdd3;
   border-radius: 8px;
--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
-  padding: 4rem 2.4rem 4rem;
-  max-width: 60.9rem;
-  background: #ffffff;
+  margin-top: 5rem;
 `
 export const Main = styled.main`
-  margin-left: 5rem;
-  margin-right: 5rem;
+  display: grid;
+  place-items: center;
 `
